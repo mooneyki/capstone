0acb4937172488ff08cd90bb5b0a4638dd27b344
kienan <mooney.ki@husky.neu.edu>
2018-11-15 11:25:54 -0500

    data and figs saved. power and eff data calc'd and plotted.

--- matlab/dyno_data_treatment.m
+++ matlab/dyno_data_treatment.m
@@ -1,3 +1,7 @@
+filename = input('Enter file name: \n');
+dir = input('Enter results directory: \n');
+mkdir (fullfile(dir));
+
 %scales, offsets
 %update this one
 torque_scale = 1;
@@ -21,7 +25,6 @@ tps_offset = 0;
 
 %parse data from files
 %each quantity has its own column
-filename = 'filename_test.csv';
 dp = csvread(filename); %contains all columns 
 num_rows = size(dp,1); %depends on test
 num_cols = size(dp,2); %should be 12
@@ -67,13 +70,17 @@ for i = 1:num_rows
     tps(i,1) = ( counts_to_volts ( tps(i,1) ) * tps_scale + tps_offset );
 end
 
+%calculate data
+engine_power = torque .* prim_rpm / 5252;
+wheel_power = load_cell .* sec_rpm / 5252;
+powertrain_efficiency = wheel_power ./ engine_power;
+
 %plot data
 x = [1:num_rows];
-%x = x(:);
 scrsz = get(0,'ScreenSize');
 
 %RPM
-figure('Position',[1 scrsz(4)/2-80 scrsz(3)/2 scrsz(4)/2])
+f = figure('Position',[1 scrsz(4)/2-80 scrsz(3)/2 scrsz(4)/2]);
 p = plot(x,prim_rpm,x,sec_rpm);
 p(1).Color = [1,0,0];
 p(2).Color = [0,1,0];
@@ -81,9 +88,11 @@ title('RPM')
 xlabel('time')
 ylabel('RPM')
 legend('Primary','Secondary')
+fullFileName = fullfile(dir,'RPM.jpg');
+saveas(f,fullFileName);
 
 %Temperatures
-figure('Position',[1 1 scrsz(3)/2 scrsz(4)/2])
+f = figure('Position',[1 1 scrsz(3)/2 scrsz(4)/2]);
 p = plot(x,temp1,x,temp2,x,temp3,x,belt_temp);
 p(1).Color = [1,0,0];
 p(2).Color = [0,1,0];
@@ -93,9 +102,11 @@ title('Temperatures')
 xlabel('time')
 ylabel('Temperature')
 legend('Temp1','Temp2','Temp3','Belt Temp')
+fullFileName = fullfile(dir,'temps.jpg');
+saveas(f,fullFileName);
 
 %Torques
-figure('Position',[scrsz(3)/2 2*scrsz(4)/3-80 scrsz(3)/2 scrsz(4)/3])
+f = figure('Position',[scrsz(3)/2 2*scrsz(4)/3-80 scrsz(3)/2 scrsz(4)/3]);
 p = plot(x,torque,x,load_cell);
 p(1).Color = [1,0,0];
 p(2).Color = [0,1,0];
@@ -103,24 +114,58 @@ title('Torque')
 xlabel('time')
 ylabel('Torque')
 legend('Primary','Secondary')
+fullFileName = fullfile(dir,'torque.jpg');
+saveas(f,fullFileName);
 
 %Brake current
-figure('Position',[scrsz(3)/2 scrsz(4)/3 scrsz(3)/2 scrsz(4)/3])
+f = figure('Position',[scrsz(3)/2 scrsz(4)/3 scrsz(3)/2 scrsz(4)/3]);
 p = plot(x,torque);
 title('Brake Current')
 xlabel('time')
 ylabel('Current')
+fullFileName = fullfile(dir,'brake current.jpg');
+saveas(f,fullFileName);
 
 %Setpoints
-figure('Position',[scrsz(3)/2 1 scrsz(3)/2 scrsz(4)/3])
+f = figure('Position',[scrsz(3)/2 1 scrsz(3)/2 scrsz(4)/3]);
 ax1 = subplot(2,1,1);
-plot(ax1,x,i_sp)
+plot(ax1,x,i_sp);
 title(ax1,'Current setpoint')
 xlabel(ax1,'time')
 ylabel(ax1,'Set point (0-100%)')
 
 ax2 = subplot(2,1,2);
-plot(ax2,x,tps_sp)
+plot(ax2,x,tps_sp);
 title(ax2,'Throttle setpoint')
 xlabel(ax2,'time')
 ylabel(ax2,'Set point (0-100%)')
+fullFileName = fullfile(dir,'setpoints.jpg');
+saveas(f,fullFileName);
+
+%Power
+f = figure('Position',[scrsz(3)/2 1 scrsz(3)/2 scrsz(4)/3]);
+ax1 = subplot(2,1,1);
+plot(ax1,x,engine_power);
+title(ax1,'Engine power')
+xlabel(ax1,'time')
+ylabel(ax1,'HP')
+
+ax2 = subplot(2,1,2);
+plot(ax2,x,wheel_power);
+title(ax2,'Output Power')
+xlabel(ax2,'time')
+ylabel(ax2,'HP')
+fullFileName = fullfile(dir,'power.jpg');
+saveas(f,fullFileName);
+
+%efficiency
+f = figure('Position',[scrsz(3)/2 scrsz(4)/3 scrsz(3)/2 scrsz(4)/3]);
+p = plot(x,powertrain_efficiency);
+title('Powertrain Effeciency')
+xlabel('time')
+ylabel('%')
+fullFileName = fullfile(dir,'efficiency.jpg');
+saveas(f,fullFileName);
+
+fullFileName = fullfile(dir,'data.mat');
+save(fullFileName);
